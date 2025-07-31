408d2fcdad6a8433fe13811a19861e736db77d9e
Vickysiva-2014 <vickysiva2014@gmail.com>
2025-07-31 09:22:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -50,3 +50,57 @@ setInterval(showNextSlide, 3000);
   document.querySelectorAll('.fade-in-section').forEach(section => {
     observer.observe(section);
   });
+
+
+const cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+    const title = card.querySelector('.card-title');
+
+    title.addEventListener('click', () => {
+      // Close all other cards
+      cards.forEach(c => {
+        if (c !== card) c.classList.remove('active');
+      });
+
+      // Toggle only this card
+      card.classList.toggle('active');
+    });
+  });
+  
+  let current = 0;
+const testimonials = document.querySelectorAll(".testimonial");
+
+function showTestimonial(index) {
+  testimonials.forEach((t, i) => {
+    t.classList.remove("active");
+    if (i === index) {
+      t.classList.add("active");
+    }
+  });
+}
+
+setInterval(() => {
+  current = (current + 1) % testimonials.length;
+  showTestimonial(current);
+}, 4000); // every 4 seconds
+
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+    const increment = Math.ceil(target / 100);
+
+    if (count < target) {
+      counter.innerText = count + increment;
+      setTimeout(updateCount, 30);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
